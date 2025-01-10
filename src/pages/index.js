888103331efe230c4abe3307cc1e1b65429f543b
Aeon Yuon Miller <aym1@mail.com>
2025-01-10 22:17:43 +0100

    added dot

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,8 +24,7 @@ export default function Home() {
   const y = useTransform(scrollYProgress, [0, 1], ["0%", "85%"])
   const s2 = useTransform(scrollYProgress, [0, 1], [1, 1.4])
   const o2 = useTransform(scrollYProgress, [0.5, 1], [1, 0])
-  const r = useTransform(scrollYProgress, [0, 1], [0, 360])
-  const s0 = useTransform(scrollYProgress, [0, .8], [1, 0.5])
+  const r = useTransform(scrollYProgress, [0, 1], [0, 180])
 
 
   return (
@@ -42,7 +41,7 @@ export default function Home() {
         
       <Transition>
           <motion.div ref={container} style={{ opacity, scale }} className="bound xl" id="startseite">
-            <motion.div style={{ rotate: r, scale: s0 }} variants={zoom} initial="hidden" animate="enter" exit="exit" className="stoerer">
+            <motion.div style={{ rotate: r }} variants={zoom} initial="hidden" animate="enter" exit="exit" className="stoerer">
               Ab<br/>März 2025
             </motion.div>
           <motion.h1 variants={hlone} initial="hidden" animate="enter" exit="exit">
