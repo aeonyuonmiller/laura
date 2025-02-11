eeef763c1ef450bc6acebdc8e389706f1a3339d7
Aeon Yuon Miller <aym1@mail.com>
2025-02-11 19:10:55 +0100

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -91,7 +91,7 @@ export default function Home() {
               animate="enter"
               exit="exit"
             >
-              <img src="/laura2.jpeg" alt="portrait" />
+              <img src="/laura2.jpeg" alt="portrait" width="400px" />
               <h5>Laura Maria Müller</h5>
               <h6 className="fineprint">Musikerin und Pädagogin</h6>
             </motion.div>
